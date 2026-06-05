f1827a5a4958847a7ba5012d5b7a05460f3042bc
HSJoy <a1145536@mail.nuk.edu.tw>
2026-06-06 05:25:18 +0800

    Update UVa_10929.cpp

---
+++ Problem/UVa_10929.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+
+    while (cin >> s && s != "0") {
+        int odd = 0;
+        int even = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (i % 2 == 0)
+                odd += s[i] - '0';
+            else
+                even += s[i] - '0';
+        }
+
+        if ((odd - even) % 11 == 0)
+            cout << s << " is a multiple of 11.\n";
+        else
+            cout << s << " is not a multiple of 11.\n";
+    }
+
+    return 0;
+}
