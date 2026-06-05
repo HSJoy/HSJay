fdd6c751ea3a1ad25bf48f26468d8b3bbcb2e713
HSJoy <a1145536@mail.nuk.edu.tw>
2026-06-06 04:14:03 +0800

    Update UVa_948.cpp

--- Problem/UVa_948.cpp
+++ Problem/UVa_948.cpp
@@ -1,89 +1,65 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <map>
-#include <set>
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 
-using namespace std;
+#define MAX_FIB_COUNT 45
+#define MAX_INPUT_VAL 100000000
 
-int N, L, ans;
-vector<vector<pair<int, int>>> choices;
+long long Fib_array[MAX_FIB_COUNT];
+int Fib_count = 0;
 
-void dfs(int idx, set<int>& used) {
-    if ((int)used.size() >= ans) return;
+void build_fib() {
+    Fib_array[0] = 1;   
+    Fib_array[1] = 2;   
+    Fib_count = 2;
 
-    if (idx == N) {
-        ans = used.size();
-        return;
-    }
-
-    for (auto p : choices[idx]) {
-        bool a = used.insert(p.first).second;
-        bool b = used.insert(p.second).second;
-
-        dfs(idx + 1, used);
-
-        if (b) used.erase(p.second);
-        if (a) used.erase(p.first);
+    for (int i = 2; i < MAX_FIB_COUNT; i++) {
+        long long next = Fib_array[i - 1] + Fib_array[i - 2];
+        if (next > MAX_INPUT_VAL) break;
+        Fib_array[i] = next;
+        Fib_count++;
     }
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    build_fib();
 
-    while (cin >> N >> L) {
-        vector<string> g(N);
+    int T;
+    scanf("%d", &T);
 
-        for (int i = 0; i < N; i++)
-            cin >> g[i];
+    for (int i = 0; i < T; i++) {
+        int ori_n;
+        if (scanf("%d", &ori_n) != 1) continue;
 
-        map<string, int> id;
-        int cnt = 0;
+        int n = ori_n;
+        char fib_base[MAX_FIB_COUNT]; 
+        int result = 0;
 
-        choices.assign(N, {});
+        int start = -1;
+        for (int j = 0; j < Fib_count; j++) {
+            if (Fib_array[j] <= n) start = j;
+            else break;
+        }
 
-        for (int i = 0; i < N; i++) {
-            vector<int> pos;
+        if (start == -1) {
+            printf("%d = 0 (fib)\n", ori_n);
+            continue;
+        }
 
-            for (int j = 0; j < L; j++)
-                if (g[i][j] == '2')
-                    pos.push_back(j);
-
-            int k = pos.size();
-
-            for (int mask = 0; mask < (1 << k); mask++) {
-                string h1 = g[i];
-                string h2 = g[i];
-
-                for (int j = 0; j < L; j++) {
-                    if (g[i][j] == '0' || g[i][j] == '1')
-                        h1[j] = h2[j] = g[i][j];
-                }
-
-                for (int j = 0; j < k; j++) {
-                    if (mask & (1 << j)) {
-                        h1[pos[j]] = '0';
-                        h2[pos[j]] = '1';
-                    }
-                    else {
-                        h1[pos[j]] = '1';
-                        h2[pos[j]] = '0';
-                    }
-                }
-
-                if (!id.count(h1)) id[h1] = cnt++;
-                if (!id.count(h2)) id[h2] = cnt++;
-
-                choices[i].push_back({ id[h1], id[h2] });
+        for (int j = start; j >= 0; j--) {
+            if (n >= Fib_array[j]) {
+                fib_base[result++] = '1';
+                n -= Fib_array[j];
+            }
+            else {
+                fib_base[result++] = '0';
             }
         }
 
-        ans = 1000000;
-        set<int> used;
-        dfs(0, used);
-
-        cout << ans << '\n';
+        fib_base[result] = '\0';
+        printf("%d = %s (fib)\n", ori_n, fib_base);
     }
 
     return 0;
