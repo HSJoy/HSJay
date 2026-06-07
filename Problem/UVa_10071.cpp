86e125709721197597cc35e5f7dfa6868ce728b4
HSJoy <a1145536@mail.nuk.edu.tw>
2026-06-07 12:11:10 +0800

    Update UVa_10071.cpp

---
+++ Problem/UVa_10071.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int v, t;
+
+    while (scanf("%d %d", &v, &t) == 2) {
+        int displacement;
+        if (t == 0) {
+            displacement = 0; 
+        } else {
+            displacement = 2 * v * t; 
+        }
+        printf("%d\n", displacement);
+    }
+
+    return 0;
+}
+
